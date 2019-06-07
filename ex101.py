7d8f2e2336da83e3e3dfe5e4509aa36e29e90dc2
Kato Lefebvre <klefebvre90@gmail.com>
2019-06-07 11:31:14 -0400

    First commit

---
+++ ex101.py
@@ -0,0 +1,7 @@
+name = input("Please tell us your name: ")
+age = input("Please tell us your age: ")
+age = int(age)
+
+year_at_100 = (100 - age) + 2019
+
+print(f"Hello {name}, when you turn 100, the year will be {year_at_100}.")
---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Kato Lefebvre'
+my_age = 23 # not a lie
+my_height = 68 # inches
+my_weight = 160 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the tea.")
+
+# this line is tricky
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
